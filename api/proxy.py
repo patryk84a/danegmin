dda9269c79fe384e9386a0f56d35b9ad2058e5b3
patryk84a <62844519+patryk84a@users.noreply.github.com>
2026-08-08 16:18:04 +0200

    Add files via upload

--- api/proxy.py
+++ api/proxy.py
@@ -43,3 +43,4 @@ class handler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps({'error': str(e)}).encode('utf-8'))
+
